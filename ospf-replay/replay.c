911d0df76f9f534a3f281455862e3ac828766317
Nathan Wilder <nwilder0@yahoo.com>
2013-05-19 18:41:16 +0600

    Initial files

---
+++ ospf-replay/replay.c
@@ -0,0 +1,15 @@
+/*
+ * replay.c
+ *
+ *  Created on: May 19, 2013
+ *      Author: nathan
+ */
+
+#include <stdio.h>
+#include "replay.h"
+
+int main(void) {
+	printf("hello world\n");
+	return 0;
+}
+
---
+++ ospf-replay/replay.h
@@ -0,0 +1,13 @@
+/*
+ * replay.h
+ *
+ *  Created on: May 19, 2013
+ *      Author: nathan
+ */
+
+#ifndef REPLAY_H_
+#define REPLAY_H_
+
+
+
+#endif /* REPLAY_H_ */
